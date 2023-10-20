6ac0c453fc74b94cfba1c5c2e771933f08335685
staz <m.kobialka@healthpharm.pl>
2023-10-20 22:33:32 +0200

    Recording

--- camera.py
+++ camera.py
@@ -2,42 +2,66 @@ import cv2
 import time
 import datetime
 
-# Inicjalizacja obiektu przechwytującego wideo dla domyślnej kamery (0)
+
 cap = cv2.VideoCapture(0)
 
-# Ładowanie klasyfikatorów Haara do detekcji twarzy i pełnych sylwetek
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
 
+detection = False
+detection_stoped_time = None
+timer_started = False
+SECONDS_TO_RECORD_AFTER_DETECTION = 5
+
+frame_size = (int(cap.get(3)), int(cap.get(4)))
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+
+
 while True:
-    # Odczytanie klatki z kamery
+
     _, frame = cap.read()
 
-    # Konwersja klatki na odcienie szarości do detekcji twarzy
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Wykrywanie twarzy w klatce w odcieniach szarości
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    # Wykrywanie pełnych sylwetek w klatce w odcieniach szarości
     bodies = body_cascade.detectMultiScale(gray, 1.3, 5)
 
+    if len(faces) + len(bodies) > 0:
+        if detection:
+            timer_started = False
+        else:
+            detection = True
+            current_time = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+            out = cv2.VideoWriter(f"{current_time}.mp4", fourcc, 20, frame_size)
+            print("Started recording!")
+    elif detection:
+        if timer_started:
+            if time.time() - detection_stoped_time >= SECONDS_TO_RECORD_AFTER_DETECTION:
+                detection=False
+                timer_starded = False
+                out.release()
+                print('Stop Recording')
+        else:
+            timer_started = True
+            detection_stoped_time = time.time()
 
-    # Rysowanie prostokątów wokół wykrytych twarzy
-    for (x, y, width, height) in faces:
-        cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3)
+    if detection:
+        out.write(frame)
 
-    # Rysowanie prostokątów wokół wykrytych pełnych sylwetek
-    for (x, y, width, height) in bodies:
-        cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 0, 255), 3)
 
-    # Wyświetlenie klatki z prostokątami
+    # for (x, y, width, height) in faces:
+    #     cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3)
+    #
+    # for (x, y, width, height) in bodies:
+    #     cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 0, 255), 3)
+
     cv2.imshow("Kamera", frame)
 
-    # Jeśli naciśniesz klawisz 'q', to wyjdziesz z pętli
     if cv2.waitKey(1) == ord('q'):
         break
 
-# Zwolnienie kamery i zamknięcie wszystkich okien OpenCV
+
+out.release()
 cap.release()
 cv2.destroyAllWindows()
